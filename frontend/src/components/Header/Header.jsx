541caf14c96e038465275b4106e157088d4f1997
K-Sindhu17 <sindhukodaboina2002@gmail.com>
2024-08-07 16:05:48 +0530

    Dynamic web page

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>choose from diverse menu featuring a delicating array of dishes crafted with a finest ingredients and culinary expertise. Our mission is to satify your cravings and elevate your dining experience, one delicious meal at a time.</p>
+            <button>View menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
